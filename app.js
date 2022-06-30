5acc11ad972ec3494313902a2039d1c49a7b60e2
GOMBAULD Clément (Canal Plus Prestataire) <clement.gombauld@cpexterne.org>
2022-06-30 08:16:02 +0200

    data sanitazation and jwt in cookie and rate limit implemented

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorhandler = require('./controllers/errorController');
@@ -8,14 +12,32 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 //Middleware
+// SET SECURITUY HTTP HEADERS
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
 // 3d-party middlewware
 
 // middleware for json post requests
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitazation against no SQL injection
+app.use(mongoSanitize());
+
+//data sanitazation against XSS
+app.use(xss());
+
 // middleware for serving static file
 app.use(express.static(`${__dirname}/public`));
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -11,8 +11,20 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  ),
+  secure: process.env.NODE_ENV === 'production' ? true : false,
+  httpOnly: true,
+};
+
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
 
   return res.status(statusCode).json({
     status: 'sucess',
